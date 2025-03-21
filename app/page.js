f664665217af5d54c64443147b22b3d8d856e4de
ljis120301 <ljis120301@gmail.com>
2025-03-20 17:13:51 -0700

    add google analyitics

--- app/page.js
+++ app/page.js
@@ -7,6 +7,20 @@ export { metadata };
 export default function Home() {
   return (
     <>
+      {/* Google Analytics */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-KV6KJZGD86"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-KV6KJZGD86');
+        `}
+      </Script>
+      
       <Script id="schema-person" type="application/ld+json" dangerouslySetInnerHTML={{
         __html: JSON.stringify({
           '@context': 'https://schema.org',
